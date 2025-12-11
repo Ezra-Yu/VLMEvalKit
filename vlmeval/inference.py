7194fe4385f9b5eef3df100d436ea505b98622c8
qiaoyu <yuzhaohui1@xiaohongshu.com>
2025-12-11 21:06:58 +0800

    update

--- vlmeval/inference.py
+++ vlmeval/inference.py
@@ -179,6 +179,11 @@ def infer_data(model, model_name, work_dir, dataset, out_file, verbose=False, ap
     return model
 
 
+# Add for agent evaluation
+def _is_structured_record(v):
+    return isinstance(v, dict) and 'prediction' in v and 'extra_records' in v
+
+
 # A wrapper for infer_data, do the pre & post processing
 def infer_data_job(
     model, work_dir, model_name, dataset, verbose=False, api_nproc=4, ignore_failed=False, use_vllm=False
@@ -216,6 +221,7 @@ def infer_data_job(
         data = dataset.data
         for x in data['index']:
             assert x in data_all
+<<<<<<< HEAD
         data['prediction'] = [str(data_all[x]) for x in data['index']]
         from copy import deepcopy
         ori_predictions = deepcopy(data['prediction'])
@@ -241,6 +247,43 @@ def infer_data_job(
         data['prediction'] = predictions
         if think_flag:
             data['ori_prediction'] = ori_predictions
+=======
+        if os.getenv('SPLIT_THINK', False):
+            if all(_is_structured_record(data_all[x]) for x in data['index']):
+                prediction = [data_all[x]['prediction'] for x in data['index']]
+                extra_records = [data_all[x]['extra_records'] for x in data['index']]
+                data['extra_records'] = extra_records
+            else:
+                prediction = [str(data_all[x]) for x in data['index']]
+
+            def split_thinking(s):
+                if '</think>' in s:
+                    splits = s.split('</think>')
+                    prediction = splits[-1].strip()
+                    if len(splits) == 2 and '<think>' in splits[0]:
+                        thinking = splits[0].split('<think>')[1].strip()
+                    else:
+                        thinking = '</think>'.join(splits[:-1])
+                        thinking += '</think>'
+                        warnings.warn('Failed to parse thinking, multiple </think> tags or missing <think> tag.')
+                else:
+                    thinking = ''
+                    prediction = s
+                return (prediction, thinking)
+            split_func = model.split_thinking if hasattr(model, 'split_thinking') else split_thinking
+            print(f'Prediction format: {os.getenv("SPLIT_THINK")},splitting func: {split_func}')
+            tups = [split_func(x) for x in prediction]
+            data['prediction'] = [x[0] for x in tups]
+            data['thinking'] = [x[1] for x in tups]
+        else:
+            # data['prediction'] = [str(data_all[x]) for x in data['index']]
+            # Add for agent evaluation
+            if all(_is_structured_record(data_all[x]) for x in data['index']):
+                data['prediction'] = [data_all[x]['prediction'] for x in data['index']]
+                data['extra_records'] = [data_all[x]['extra_records'] for x in data['index']]
+            else:
+                data['prediction'] = [str(data_all[x]) for x in data['index']]
+>>>>>>> main
         if 'image' in data:
             data.pop('image')
 
